fade460df8a703e9096c5389d701c3209e23ddae
khjin001010 <khjin001010@naver.com>
2017-08-29 20:15:49 +0900

    #2 connect the firebase

--- app/src/main/java/khjin001010/kr/hs/emirim/myapplication/LoginActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/myapplication/LoginActivity.java
@@ -1,4 +1,14 @@
 package khjin001010.kr.hs.emirim.myapplication;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
 }
